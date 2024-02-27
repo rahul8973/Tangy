2e239eb7fbeb76da75ab448c8e14d61bd3250a0b
Rahul Kumar <rahulkumar.k@valtech.com>
2024-02-27 11:42:15 +0530

    Added Components for practise
    Added Components for practise

---
+++ Models/LearnBlazorModels/Demo_ProdProperties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Models.LearnBlazorModels
+{
+	public class Demo_ProdProperties
+	{
+		public int Id { get; set; }
+		public string Key { get; set; }
+		public string Value { get; set; }
+
+	}
+}
+
---
+++ Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Models.LearnBlazorModels
+{
+	public class Demo_Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public bool IsActive { get; set; }
+		public double Price { get; set; }
+		public string ActiveString
+		{
+			get => IsActive ? "Active" : "InActive";
+		}
+
+		public List<Demo_ProdProperties> ProdProperties { get; set; }
+	}
+}
+
